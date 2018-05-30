6d3873d55a83182f7ddf38e6200eb71ed5b694f1
Stefaan Ponnet <stefaan@ponnet.com>
2018-05-30 22:11:52 +0200

    added script to send tokens to bridge ( for testing validators )

---
+++ scripts/send_tokens_to_bridge.js
@@ -0,0 +1,63 @@
+const SampleERC20 = artifacts.require("./SampleERC20.sol");
+
+module.exports = function(callback) {
+
+	const Web3 = require('web3');
+
+	//const ethUtil = require("ethereumjs-util");
+	const EthereumTx = require("ethereumjs-tx");
+
+	const PrivateKeyProvider = require("truffle-privatekey-provider");
+
+	const path = require('path');
+	const configfile = path.join(__dirname, 'bridge-info.json');
+	const inputconfig = require(configfile);
+	const alice = require('./alice.json');
+
+	// let p = new PrivateKeyProvider(alice.private, "https://ropsten.infura.io/U8U4n8mm2wDgB2e3Dksv");
+	// SampleERC20.web3.setProvider(p);
+
+	// console.log('Web3', SampleERC20.Web3);
+	// console.log('web3', SampleERC20.web3);
+
+	// SampleERC20.at(inputconfig.hometoken.address).transfer(inputconfig.homebridge.address, 1, {
+	// 	from: alice.public
+	// });
+	//	process.exit();
+
+	//let tempweb3 = new Web3(new PrivateKeyProvider(alice.private, "https://ropsten.infura.io/U8U4n8mm2wDgB2e3Dksv"));
+	let tempweb3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/U8U4n8mm2wDgB2e3Dksv"));
+	homeToken = new tempweb3.eth.Contract(SampleERC20.abi, inputconfig.hometoken.address);
+
+	let data = homeToken.methods.transfer(inputconfig.homebridge.address, 1).encodeABI();
+
+	const privateKey = Buffer.from(alice.private, 'hex');
+
+	const txParams = {
+		to: inputconfig.hometoken.address,
+		data: data,
+		//gas: 150000,
+		from: alice.public,
+		//nonce: 0,
+	};
+
+	console.log(txParams);
+
+	const tx = new EthereumTx(txParams);
+	tx.sign(privateKey);
+	const serializedTx = tx.serialize();
+
+	//	console.log('serializedTx', serializedTx);
+
+	//tempweb3 = new Web3("https://ropsten.infura.io/U8U4n8mm2wDgB2e3Dksv");
+
+	tempweb3.eth.sendSignedTransaction(serializedTx, function(err, tx) {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log('tx', tx);
+		}
+
+	});
+};
+//
--- scripts/show-validatorcmds.js
+++ scripts/show-validatorcmds.js
@@ -1,5 +1,15 @@
-w1 = require('./w1.json');
-w2 = require('./w2.json');
-w3 = require('./w3.json');
+const w1 = require('./w1.json');
+const w2 = require('./w2.json');
+const w3 = require('./w3.json');
+const bridge = require('./bridge-info.json');
 
-console.log('3,["' + w1.public + '","' + w2.public + '","' + w3.public + '"]');
+const endpoint = 'wss://ropsten.infura.io/ws';
+
+console.log('node bin.js --mainweb3hostws ' + endpoint 
+	+ ' --foreignweb3hostws '+ endpoint
+	+ ' --maincontractaddress ' + bridge.homebridge.address
+	+ ' --foreigncontractaddress ' + bridge.foreignbridge.address
+	+ ' --startblock 3340495');
+
+
+//console.log('3,["' + w1.public + '","' + w2.public + '","' + w3.public + '"]');
