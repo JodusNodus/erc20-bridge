1baa4467f89c59ef639b7c930694d264a367f47c
JodusNodus <billiet.thomas@outlook.com>
2018-07-18 12:30:22 +0200

    improve testrpc

--- scripts/testrpc.js
+++ scripts/testrpc.js
@@ -4,12 +4,21 @@ const HDWalletProvider = require('truffle-hdwallet-provider')
 
 const seedToPrivateKey = (seed) => {
   const provider = new HDWalletProvider(seed)
-  return '0x' + provider.hdwallet._hdkey._privateKey.toString("hex");
+  const addr = provider.addresses[0];
+  const private = provider.wallets[addr].getPrivateKeyString();
+
+  console.log("---")
+  console.log("SEED:", seed);
+  console.log("ADDRESS:", addr);
+  console.log("PRIVATE KEY:", private);
+  console.log("---")
+
+  return private;
 }
 
 const seedToAccountBalance = (seed) => ({
   secretKey: seedToPrivateKey(seed),
-  balance: 0x2386F26FC10000
+  balance: 1e30
 })
 
 
@@ -25,7 +34,8 @@ const serverConf = {
 
 const server = ganache.server(serverConf);
 
-
 server.listen(8545, function (err, blockchain) {
-
+  if (err) {
+    console.error(err);
+  }
 });
